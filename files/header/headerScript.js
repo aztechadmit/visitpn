0ede18953e028ea212ae5ce83c87ddf658696464
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-07-24 16:57:41 -0700

    Update headerScript.js

--- files/header/headerScript.js
+++ files/header/headerScript.js
@@ -4,6 +4,9 @@ const loader = document.getElementById("loader");
 
 const linkHead = "https://aztechadmit.github.io/visitpn/";
 
+var screenSize = '';
+var menuOpen = 'none';
+
 function endPageLoad(){
 	loader.style.opacity = 0;
 	setTimeout(function(){loader.style.display='none'},1000);
@@ -12,13 +15,19 @@ function endPageLoad(){
 function createHeader(){
 	if(window.innerWidth > 650){ 	// Large Window
 		header.innerHTML = "<div id='headerBar'> <img src='"+linkHead+"files/images/puertasNuevasLogoII.png' id='headerLogo' onclick='window.open("+'"'+linkHead+'"'+")'> \
-							<div id='headerContent'><a>Places to Visit</a> <a>Things to Do</a> <a>Plan Your Visit</a> <a>Information</a></div> </div>";
-		
+							<div id='headerContent'><a href='"+linkHead+"places-to-visit'>Places to Visit</a> <a onhover='openTTD'>Things to Do</a> <a href='"+linkHead+"planning'>Plan Your Visit</a> <a href='"+linkHead+"information'>Information</a></div> </div>";
+		screenSize = 'large';
 	}else{				// Small Window
 		header.innerHTML = "<div id='headerBar'> <img src='"+linkHead+"files/images/puertasNuevasLogoII.png' id='headerLogo' onclick='window.open("+'"'+linkHead+'"'+")'> \
-							<div id='headerContent'><a>Menu<a></div> </div>";
+							<div id='headerContent'><a>Menu</a></div> </div>";
+		screenSize = 'small';
 	}
 }// end of function createHeader()
 
 // Resize header if the window size changes
 window.onresize = function(){createHeader();};
+
+function openTTD(){
+	const headerBar = document.getElementById("headerBar");
+	headerBar.innerHTML += "<div id='menuContent'> <a>Attractions</a> <a>Restaurants</a> <a>Entertainment</a> <a>Museums & Art</a> </div>";
+}
