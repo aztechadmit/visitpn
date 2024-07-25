02c0a3a9fae7820ec751167a1b1deb967f902bcf
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-07-24 17:00:51 -0700

    Update headerScript.js

--- files/header/headerScript.js
+++ files/header/headerScript.js
@@ -15,11 +15,12 @@ function endPageLoad(){
 function createHeader(){
 	if(window.innerWidth > 650){ 	// Large Window
 		header.innerHTML = "<div id='headerBar'> <img src='"+linkHead+"files/images/puertasNuevasLogoII.png' id='headerLogo' onclick='window.open("+'"'+linkHead+'"'+")'> \
-							<div id='headerContent'><a href='"+linkHead+"places-to-visit'>Places to Visit</a> <a onhover='openTTD'>Things to Do</a> <a href='"+linkHead+"planning'>Plan Your Visit</a> <a href='"+linkHead+"information'>Information</a></div> </div>";
+							<div id='headerContent'><a href='"+linkHead+"places-to-visit'>Places to Visit</a> <a onhover='openTTD'>Things to Do</a> <a href='"+linkHead+"planning'>Plan Your Visit</a> <a href='"+linkHead+"information'>Information</a></div> \
+							<div id='menuContent'></div> </div>";
 		screenSize = 'large';
 	}else{				// Small Window
 		header.innerHTML = "<div id='headerBar'> <img src='"+linkHead+"files/images/puertasNuevasLogoII.png' id='headerLogo' onclick='window.open("+'"'+linkHead+'"'+")'> \
-							<div id='headerContent'><a>Menu</a></div> </div>";
+							<div id='headerContent'><a>Menu</a></div> <div id='menuContent'></div> </div>";
 		screenSize = 'small';
 	}
 }// end of function createHeader()
@@ -29,6 +30,7 @@ window.onresize = function(){createHeader();};
 
 function openTTD(){
 	const headerBar = document.getElementById("headerBar");
-	headerBar.innerHTML += "<div id='menuContent'> <a>Attractions</a> <a>Restaurants</a> <a>Entertainment</a> <a>Museums & Art</a> </div>";
-	document.getElementById("menuContent").style.opacity = 1;
+	menuContent.innerHTML = "<a>Attractions</a> <a>Restaurants</a> <a>Entertainment</a> <a>Museums & Art</a>";
+	menuContent.style.display = 'block';
+	setTimeout(function(){document.getElementById("menuContent").style.opacity = 1;},100);
 }
