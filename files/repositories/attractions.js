8389551f1f62af102bbb19a1daf2d7dbf3d3a7d8
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-08-01 15:02:01 -0700

    Update attractions.js

--- files/repositories/attractions.js
+++ files/repositories/attractions.js
@@ -1,1 +1,2 @@
-
+categories = ["none", "Attractions", "Site Seeing", "Restaurants", "Casinos", "Entertainment", "Sports & Recreation", "Museums & Galleries"];
+categoryImg = ["none", "menuIcon.png", "menuIcon(11).png", "menuIcon(1).png", "menuIcon(5).png", "menuIcon(3).png", "menuIcon(7).png", "menuIcon(4).png"];
