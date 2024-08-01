71401317b91c3323e2d3f204f46806965bd089e3
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-08-01 15:11:13 -0700

    Update attractions.js

--- files/repositories/attractions.js
+++ files/repositories/attractions.js
@@ -1,2 +1,4 @@
 categories = ["none", "Attractions", "Site Seeing", "Restaurants", "Casinos", "Entertainment", "Sports & Recreation", "Museums & Galleries"];
 categoryImg = ["none", "menuIcon.png", "menuIcon(11).png", "menuIcon(1).png", "menuIcon(5).png", "menuIcon(3).png", "menuIcon(7).png", "menuIcon(4).png"];
+locations = ["none", "Puerta Azul", "Northport", "Port City", "Bosque Viejo", "Playa Del Sol"];
+
