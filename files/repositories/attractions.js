dab0ea261ccad1216b48e1fe5ea59cc9cf81cd74
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-08-01 16:50:16 -0700

    Update attractions.js

--- files/repositories/attractions.js
+++ files/repositories/attractions.js
@@ -1,4 +1,4 @@
-categories = ["none", "Attractions", "Site Seeing", "Restaurants", "Shopping & Dining", "Casinos", "Entertainment", "Sports & Recreation", "Museums & Galleries"];
+categories = ["none", "Attractions", "Site Seeing", "Restaurants", "Shopping & Snacking", "Casinos", "Entertainment", "Sports & Recreation", "Museums & Galleries"];
 categoryImg = ["none", "menuIcon.png", "menuIcon(11).png", "menuIcon(1).png", "menuIcon(8).png", "menuIcon(5).png", "menuIcon(3).png", "menuIcon(7).png", "menuIcon(4).png"];
 locations = ["none", "Puerta Azul", "Northport", "Port City", "Bosque Viejo", "Playa Del Sol"];
 
@@ -7,7 +7,7 @@ locations = ["none", "Puerta Azul", "Northport", "Port City", "Bosque Viejo", "P
 	attractions = [];
 	attractions[0] = "Do not use";
 
-	attractions[1] = "Northport Village | Northport | Shopping & Dining | A preppy outdoor shopping, dining, and living community along the western coast | attractions?index=0 | https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg";
+	attractions[1] = "Northport Village | Northport | Shopping & Snacking | A preppy outdoor shopping, dining, and living community along the western coast | attractions?index=0 | https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg";
 	attractions[2] = "Burke Aquarium | Northport | Attractions | A grand and wonderous aquarium featuring hundreds of ocean creatures and engaging exhibits | attractions?index=0 | https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg";
 	attractions[3] = "MC Resort of Puerta Azul | Puerta Azul | Casinos | Minecraftia's largest casino resort with towering hotel rooms, multi-floor gaming halls, and grand ballrooms | attractions?index=0 | https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg";
-  
+  	attractions[4] = "RE 5 | Northport | Casinos | Reyman Estates hotel #5 featuring luxury rooms, a rooftop pool, and a flexible convention & meeting space | attractions?index=0 | https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg";
