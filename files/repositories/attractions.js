2f2d6f6272053e97271ce7e5937c31b8346e704b
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-08-01 16:13:28 -0700

    Update attractions.js

--- files/repositories/attractions.js
+++ files/repositories/attractions.js
@@ -1,4 +1,13 @@
-categories = ["none", "Attractions", "Site Seeing", "Restaurants", "Casinos", "Entertainment", "Sports & Recreation", "Museums & Galleries"];
-categoryImg = ["none", "menuIcon.png", "menuIcon(11).png", "menuIcon(1).png", "menuIcon(5).png", "menuIcon(3).png", "menuIcon(7).png", "menuIcon(4).png"];
+categories = ["none", "Attractions", "Site Seeing", "Restaurants", "Shopping & Dining", "Casinos", "Entertainment", "Sports & Recreation", "Museums & Galleries"];
+categoryImg = ["none", "menuIcon.png", "menuIcon(11).png", "menuIcon(1).png", "menuIcon(8).png", "menuIcon(5).png", "menuIcon(3).png", "menuIcon(7).png", "menuIcon(4).png"];
 locations = ["none", "Puerta Azul", "Northport", "Port City", "Bosque Viejo", "Playa Del Sol"];
 
+// attractions[NUM] = "attraction_name | attraction_location | attraction_category | attraction_description | attraction_link | attraction_image_link";
+// default link: attractions?index=0
+	attractions = [];
+	attractions[0] = "Do not use";
+
+	attractions[1] = "Northport Village | Northport | Shopping & Dining | A preppy outdoor shopping, dining, and living community along the western coast | attractions?index=0 | https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg";
+	attractions[2] = "Burke Aquarium | Northport | Attractions | A grand and wonderous aquarium featuring hundreds of ocean creatures and engaging exhibits | attractions?index=0 | https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg";
+	attractions[3] = "MC Resort of Puerta Azul | Puerta Azul | Casinos | Minecraftia's largest casino resort with towering hotel rooms, multi-floor gaming halls, and grand ballrooms | attractions?index=0 | https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg";
+  
