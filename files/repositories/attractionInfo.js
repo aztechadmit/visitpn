05238777d246b599a9876fd3959c6d1d4ab8af44
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-08-05 16:44:29 -0700

    Update attractionInfo.js

--- files/repositories/attractionInfo.js
+++ files/repositories/attractionInfo.js
@@ -6,6 +6,7 @@ attractionInfo = [];attractionSummary = [];attractionDescription = [];attraction
         attractionInfo[NUM] = "ATTRACTION_NAME | ATTRACTION_CATEGORY | ATTRACTION_LOCATION"
         attractionSummary[NUM] = "Intro_to_Attraction";
         attractionDescription[NUM] = "Detailed_Info_About_Attraction";
+        attractionHighlight[NUM] = "Transit_Information";
         attractionImages[NUM] = "banner_image_link | nan";
         attractionSimilar[NUM] = "nan";
 */
@@ -13,6 +14,7 @@ attractionInfo = [];attractionSummary = [];attractionDescription = [];attraction
         attractionInfo[0] = "ATTRACTION_NAME | ATTRACTION_CATEGORY | ATTRACTION_LOCATION"
         attractionSummary[0] = "Intro_to_Attraction";
         attractionDescription[0] = "Detailed_Info_About_Attraction";
+        attractionHighlight[0] = "Transit_Information";
         attractionImages[0] = "banner_image_link | nan";
         attractionSimilar[0] = "nan";
 
@@ -22,7 +24,8 @@ attractionInfo = [];attractionSummary = [];attractionDescription = [];attraction
         attractionDescription[1] = "<h4>Discover Northport Village</h4>Northport Village is the largest development project by Reyman Estates, designed to rejuvenate the northern section of Port City. Conceived by renowned theme park engineer Rooveen Bayman, the village combines a vibrant and aesthetic charm with cozy corridors and exciting surprises around every corner. Spend the day at the village indulging in a diverse selection of food and drinks, and reinvent yourself at a wide array of stores and boutiques. Work up an appetite at the luxury gym or discover a new story at World of Books. \
                                         <h4>Relax at Northport Beach</h4>One of the highlights of Northport Village is its expansive beach, offering visitors a perfect spot to relax and unwind. The beach is equipped with amenities such as lounge chairs, umbrellas, volleyball courts, fire-pits, and lifeguard stations to ensure a safe and enjoyable experience for all visitors.\
                                         <h4>Port-City's New Sibling</h4>In 2020, the Puertas Nuevas government sought to transform the dilapidated farming area in northern Port City into a beautiful, vibrant community to attract visitors. Reyman Estates Property Developments was tasked with this transformation, bringing in legendary theme park designer Rooveen Bayman to envision the perfect seaside town. The area was incorporated into the new city of Northport, giving birth to a pristine, stunning neighborhood for living, shopping, and working. \
-                                        <div class='spacer'></div> <h3>Parking and Transportation</h3>Northport Village is easily accessible via the 305 bus line and the Sunset transit line. Conveniently located north of Highway 1, the village can be reached via Northport Ave. <p><b>Paid Parking</b> is available via PNPC (Puertas Nuevas Parking Company) parking structures. Rates vary with a daily maximum of $24. Users can pay via the ONPC mobile app or with credit card; <b>Cash is not accepted</b>.";
+                                        ";
+        attractionHighlight[1] = "<h3>Parking and Transportation</h3>Northport Village is easily accessible via the 305 bus line and the Sunset transit line. Conveniently located north of Highway 1, the village can be reached via Northport Ave. <p><b>Paid Parking</b> is available via PNPC (Puertas Nuevas Parking Company) parking structures. Rates vary with a daily maximum of $24. Users can pay via the ONPC mobile app or with credit card; <b>Cash is not accepted</b>.";
         attractionImages[1] = "https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg | https://i.ytimg.com/vi/P6-VHpdg4FQ/maxresdefault.jpg | https://i.redd.it/q4fjauk2ebc31.png";
         attractionSimilar[1] = "nan";
 
