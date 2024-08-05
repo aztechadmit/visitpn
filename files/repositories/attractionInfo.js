926957692816fee434389c73acd7a7bd511d0626
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-08-05 11:44:40 -0700

    Update attractionInfo.js

--- files/repositories/attractionInfo.js
+++ files/repositories/attractionInfo.js
@@ -1,8 +1,29 @@
 attractionInfo = [];
 /*
+        placeholder: https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg
+
         attractionInfo[NUM] = "ATTRACTION_NAME | ATTRACTION_CATEGORY | ATTRACTION_LOCATION"
         attractionSummary[NUM] = "Intro_to_Attraction";
         attractionDescription[NUM] = "Detailed_Info_About_Attraction";
         attractionImages[NUM] = "banner_image_link | nan";
         attractionSimilar[NUM] = "nan";
 */
+
+        attractionInfo[0] = "ATTRACTION_NAME | ATTRACTION_CATEGORY | ATTRACTION_LOCATION"
+        attractionSummary[0] = "Intro_to_Attraction";
+        attractionDescription[0] = "Detailed_Info_About_Attraction";
+        attractionImages[0] = "banner_image_link | nan";
+        attractionSimilar[0] = "nan";
+
+
+        attractionInfo[1] = "Northport Village | Shopping & Snacking | Northport"
+        attractionSummary[1] = "Explore a diverse selection of shopping and dining offerings in a gorgeous beach-side village. With quaint corridoors and a cool sea-breeze, enjoy the outdoors while you stroll through tree lined plazas and past colorful storefronts.";
+        attractionDescription[1] = "Northport Village was the largest development project of company Reyman Estates to revitalize the northern section of Port-city. Designed by a seasoned Theme-park engineer, the village has a colorful and asthetic look and offers a feeling of comfort in its cozy corridoors and excitement around every corner. \
+                                       <p>Spend the day at the village with a diverse selection of food and drink and re-invent yourself at a wide array of stores and boutiques. Work up an apettite at the luxury gym or find a new story to invest at World of Books.</p> \
+                                        <h4>Port-City's New Sibling</h4>In 2020, the Puertas Nuevas government wanted to re-utilize the delapidated farming village in northern port-city to beautify the city and attract visitors. Reyman Estates Property Developments was tasked with transforming the area and brought in legendary Theme-Park designer Rooveen Bayman to imagine the perfect beach-side town. The area was dissolved into the new city of Northport and a pristine and gorgeous living-shopping-working neighborhood was born. \
+                                        <div class='spacer'></div> <h3>Parking and Transportation</h3>Northport village can be reached by the 305 busline and is accessible via the Sunset transit line. The village is north of Highway 1 and can be reached via Northport Ave. <p><b>Paid Parking</b> is available via PNPC (Puertas Nuevas Parking Company) parking structures. Rates varry with a daily maximum of $24. Users can pay via the ONPC mobile app or with credit card, <b>Cash is not accepted</b>.";
+        attractionImages[1] = "https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg | nan";
+        attractionSimilar[1] = "nan";
+
+
+
