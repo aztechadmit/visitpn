d4e2f6e6b56aceaf6dda580f9bd9bc20694770de
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-08-05 09:40:04 -0700

    Create attractionInfo.js

---
+++ files/repositories/attractionInfo.js
@@ -0,0 +1,8 @@
+attractionInfo = [];
+/*
+        attractionInfo[NUM] = "ATTRACTION_NAME | ATTRACTION_CATEGORY | ATTRACTION_LOCATION"
+        attractionSummary[NUM] = "Intro_to_Attraction";
+        attractionDescription[NUM] = "Detailed_Info_About_Attraction";
+        attractionImages[NUM] = "banner_image_link | nan";
+        attractionSimilar[NUM] = "nan";
+*/
