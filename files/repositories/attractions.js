28cef2da17186c7b1e4ea58a19416360e88f27dd
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-08-07 09:43:12 -0700

    Update attractions.js

--- files/repositories/attractions.js
+++ files/repositories/attractions.js
@@ -1,6 +1,6 @@
 categories = ["none", "Attractions", "Site Seeing", "Restaurants", "Shopping & Snacking", "Casinos", "Entertainment", "Sports & Recreation", "Museums & Galleries"];
 categoryImg = ["none", "menuIcon.png", "menuIcon(11).png", "menuIcon(1).png", "menuIcon(8).png", "menuIcon(5).png", "menuIcon(3).png", "menuIcon(7).png", "menuIcon(4).png"];
-locations = ["none", "Puerta Azul", "Northport", "Port City", "Bosque Viejo", "Playa Del Sol"];
+locations = ["none", "Puerta Azul", "Northport", "Port City", "Bosque Viejo", "Playa Del Sol", "Eastlands"];
 
 // attractions[NUM] = "attraction_name | attraction_location | attraction_category | attraction_description | attraction_link | attraction_image_link";
 // default link: attractions?index=0
@@ -11,3 +11,4 @@ locations = ["none", "Puerta Azul", "Northport", "Port City", "Bosque Viejo", "P
 	attractions[2] = "Burke Aquarium | Northport | Attractions | A grand and wonderous aquarium featuring hundreds of ocean creatures and engaging exhibits | attractions?index=0 | https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg";
 	attractions[3] = "MC Resort of Puerta Azul | Puerta Azul | Casinos | Minecraftia's largest casino resort with towering hotel rooms, multi-floor gaming halls, and grand ballrooms | attractions?index=0 | https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg";
   	attractions[4] = "RE Resort of Northport | Northport | Casinos | Reyman Estates hotel #5 featuring luxury rooms, a rooftop pool, and a flexible convention & meeting space | attractions?index=0 | https://aztechadmit.github.io/visitpn/files/images/placeholder.jpg";
+	attractions[5] = "Eastlands Quarry & Restone Mine | Eastlands | Site Seeing | A historic Quarry from decades past which has been overtaken by nature. Features a small museum about the history of PN mining. | attractions?index=2 | https://staticg.sportskeeda.com/editor/2023/08/88567-16927566943270-1920.jpg";
