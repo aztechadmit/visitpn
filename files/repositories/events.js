77126af1c32e41de425be34735d0a6ae28c99d40
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-08-08 09:15:16 -0700

    Create events.js

---
+++ files/repositories/events.js
@@ -0,0 +1,10 @@
+events = []; eventsInfo = []; eventsImages = [];
+
+eventsCategory = ["none", "Music Festival", "Seasonal", "Cultural Festival", "Concerts", "Charity"];
+
+/*
+	events[NUM] = "event_name | event_months_comma_list | events_category | events_location | event_banner_image | event_description | event_click_link";
+	eventsInfo[NUM] = "event_extended_description | full_event_description | similar_events_or_nan";
+	eventsImages[NUM] = "image_one_or_nan | image_link_two";
+
+*/
